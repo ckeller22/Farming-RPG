91915493b382eb963f06657b92ecc034c450eb8a
ckeller22 <chriskeller222@gmail.com>
2020-09-30 09:28:45 -0700

    Implemented scenery objects. Added BoundsConfiner for player camera.

---
+++ Assets/Scripts/Scene/SwitchConfineBoundingShape.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class SwitchConfineBoundingShape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchBoundingShape();
+    }
+
+    /// <summary>
+    /// Switch the collider that cinemachine uses to define the dedges of the screen.
+    /// </summary>
+    private void SwitchBoundingShape()
+    {
+        PolygonCollider2D polygonCollider2D = GameObject.FindGameObjectWithTag(Tags.BoundsConfiner).GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+
+        cinemachineConfiner.InvalidatePathCache();
+    }
+
+}
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Clone of popular farming simulator/RPGS.
 
 # Recent Progress:
-![image](https://user-images.githubusercontent.com/39716848/94485933-88b02e80-0193-11eb-9406-a442dba69435.png)
+![image](https://user-images.githubusercontent.com/39716848/94713076-2ffda480-02ff-11eb-97d8-4e732615277a.png)
+
+
