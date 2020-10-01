91d8a509f937dce55a33ae35f75a26d7836747e1
ckeller22 <chriskeller222@gmail.com>
2020-10-01 14:15:19 -0700

    More work on items/inventory. Implemented item nudge animation.

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{   
+    [ItemCodeDescription]
+    [SerializeField]
+    private int _ItemCode;
+
+    private SpriteRenderer spriteRenderer;
+
+    public int ItemCode { get { return _ItemCode; } set { _ItemCode = value; } }
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        if (ItemCode != 0)
+        {
+            Init(ItemCode);
+        }
+    }
+
+    public void Init(int itemCodeParam)
+    {
+        if (itemCodeParam != 0)
+        {
+            ItemCode = itemCodeParam;
+
+            ItemDetails itemDetails = InventoryManager.Instance.GetItemDetails(ItemCode);
+            spriteRenderer.sprite = itemDetails.itemSprite;
+            if (itemDetails.itemType == ItemType.ReapableScenery)
+            {
+                gameObject.AddComponent<ItemNudge>();
+            }
+        }
+    }
+
+
+}
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Clone of popular farming simulator/RPGS.
 
 # Recent Progress:
+[![Image from Gyazo](https://i.gyazo.com/5a6a07bbd6150a4bbdf68646ad5f3408.gif)](https://gyazo.com/5a6a07bbd6150a4bbdf68646ad5f3408)
+
 ![image](https://user-images.githubusercontent.com/39716848/94713076-2ffda480-02ff-11eb-97d8-4e732615277a.png)
 
 
